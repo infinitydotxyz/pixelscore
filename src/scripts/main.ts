5fa8d47e20adae81d9881752e27bfce7b0f5ec76
nneverlander <findadit@gmail.com>
2022-10-08 17:28:48 +0200

    changes

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -75,7 +75,7 @@ async function run(chainId: string, address: string, retries: number, retryAfter
     console.log(
       `============ Fetching data for ${chainId}:${address} with max ${retries} retries and ${retryAfter} second retry interval ============`
     );
-    const collectionDoc = await infinityDb.collection('collections').doc(`${chainId}:${address}`).get();
+    // const collectionDoc = await infinityDb.collection('collections').doc(`${chainId}:${address}`).get();
     // check if collection is already downloaded to local file system
     const collectionDir = path.join(DATA_DIR, address);
     if (retries === origRetries && existsSync(collectionDir)) {
@@ -84,11 +84,11 @@ async function run(chainId: string, address: string, retries: number, retryAfter
     }
 
     // check if collection indexing is complete
-    const status = collectionDoc?.data()?.state.create.step;
-    if (status !== 'complete') {
-      console.error('Collection indexing is not complete for', address);
-      return;
-    }
+    // const status = collectionDoc?.data()?.state.create.step;
+    // if (status !== 'complete') {
+    //   console.error('Collection indexing is not complete for', address);
+    //   return;
+    // }
 
     // exception for ENS and unstoppable domains
     if (
